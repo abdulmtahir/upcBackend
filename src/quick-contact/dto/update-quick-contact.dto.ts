54dfae579514bcc0f82393dbce8be1da2c4b2638
abdulmtahir <taheer2005@yahoo.com>
2023-08-01 11:06:17 +0100

    Qcontact removed

--- src/quick-contact/dto/update-quick-contact.dto.ts
+++
@@ -1,4 +0,0 @@
-import { PartialType } from '@nestjs/mapped-types';
-import { CreateQuickContactDto } from './create-quick-contact.dto';
-
-export class UpdateQuickContactDto extends PartialType(CreateQuickContactDto) {}
--- src/quick-contact/quick-contact.module.ts
+++
@@ -1,9 +0,0 @@
-import { Module } from '@nestjs/common';
-import { QuickContactService } from './quick-contact.service';
-import { QuickContactController } from './quick-contact.controller';
-
-@Module({
-  controllers: [QuickContactController],
-  providers: [QuickContactService]
-})
-export class QuickContactModule {}
